436ebfa9690237e295f274a2d48163b4efb0a9ee
Filip Niklas <118931755+Firgrep@users.noreply.github.com>
2024-06-02 15:12:13 +0200

    feat: handlers for upload and deletion

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -16,53 +17,21 @@ import (
 
 var REQUIRED_VARS = []string{"PORT", "GCP_PRIMARY_BUCKET_NAME", "GCP_SECONDARY_BUCKET_NAME"}
 
-func generateV4GetObjectSignedURL(w io.Writer, bucket, object string) (string, error) {
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		return "", fmt.Errorf("storage.NewClient: %w", err)
-	}
-	defer client.Close()
-
-	// Signing a URL requires credentials authorized to sign a URL. You can pass
-	// these in through SignedURLOptions with one of the following options:
-	//    a. a Google service account private key, obtainable from the Google Developers Console
-	//    b. a Google Access ID with iam.serviceAccounts.signBlob permissions
-	//    c. a SignBytes function implementing custom signing.
-	// In this example, none of these options are used, which means the SignedURL
-	// function attempts to use the same authentication that was used to instantiate
-	// the Storage client. This authentication must include a private key or have
-	// iam.serviceAccounts.signBlob permissions.
-	opts := &storage.SignedURLOptions{
-		Scheme:  storage.SigningSchemeV4,
-		Method:  "GET",
-		Expires: time.Now().Add(15 * time.Minute),
-	}
-
-	blob := client.Bucket(bucket).Object(object)
-
-	// Check if the object exists
-	_, err = blob.Attrs(ctx)
-
-	if err != nil {
-		fmt.Fprintf(w, "No object found: %s", http.StatusText(http.StatusNotFound))
-		return "", nil
-	}
-
-	u, err := client.Bucket(bucket).SignedURL(object, opts)
-	if err != nil {
-		return "Error creating signed URL", fmt.Errorf("Bucket(%q).SignedURL: %w", bucket, err)
-	}
-
-	fmt.Fprintln(w, "Generated GET signed URL:")
-	fmt.Fprintf(w, "%q\n", u)
-	fmt.Fprintln(w, "You can use this URL with any user agent, for example:")
-	fmt.Fprintf(w, "curl %q\n", u)
-	return u, nil
+type StorageClient interface {
+	Bucket(name string) *storage.BucketHandle
 }
+
+type ReadObjectRequest struct {
+	Object string `json:"object"`
+}
+
+type WriteObjectRequest struct {
+	Object string `json:"object"`
+}
+
 func main() {
 	/*
-		Validate and load environment variables
+		Load and validate environment variables
 	*/
 	err := godotenv.Load()
 	if err != nil {
@@ -82,16 +51,171 @@ func main() {
 	port := os.Getenv("PORT")
 	bucket := os.Getenv("GCP_PRIMARY_BUCKET_NAME")
 	/*
-		Start server
+		Initialize Google Cloud Storage client
 	*/
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		object := r.URL.Path[1:] // Get the object from the request URL path
-		if object == "" {
-			http.Error(w, "Object not found.", http.StatusNotFound)
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		errorMessage := fmt.Errorf("storage.NewClient: %w", err)
+		log.Fatal(errorMessage)
+	}
+	defer client.Close()
+	/*
+		Define server routes
+	*/
+	http.HandleFunc("/read-object", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
 			return
 		}
-		generateV4GetObjectSignedURL(w, bucket, object)
+		// Read the request body
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		// Unmarshal the JSON data into a Go struct
+		var requestBody ReadObjectRequest
+		err = json.Unmarshal(body, &requestBody)
+		if err != nil {
+			http.Error(w, "Unable to parse JSON", http.StatusBadRequest)
+			return
+		}
+		object := requestBody.Object
+		generateV4GetObjectSignedURL(w, bucket, object, client, ctx)
 	})
+	http.HandleFunc("/write-object", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		// Read the request body
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		// Unmarshal the JSON data into a Go struct
+		var requestBody WriteObjectRequest
+		err = json.Unmarshal(body, &requestBody)
+		if err != nil {
+			http.Error(w, "Unable to parse JSON", http.StatusBadRequest)
+			return
+		}
+		object := requestBody.Object
+		generateV4PutObjectSignedURL(w, bucket, object, client)
+	})
+	http.HandleFunc("/delete-object", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		// Read the request body
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		// Unmarshal the JSON data into a Go struct
+		var requestBody WriteObjectRequest
+		err = json.Unmarshal(body, &requestBody)
+		if err != nil {
+			http.Error(w, "Unable to parse JSON", http.StatusBadRequest)
+			return
+		}
+		object := requestBody.Object
+		deleteObject(w, bucket, object, client, ctx)
+	})
+	/*
+		Start server
+	*/
 	fmt.Println("[Server] Listening on port 8080")
 	http.ListenAndServe(":"+port, nil)
+}
+
+func generateV4GetObjectSignedURL(w http.ResponseWriter, bucket, object string, client StorageClient, ctx context.Context) (string, error) {
+	opts := &storage.SignedURLOptions{
+		Scheme:  storage.SigningSchemeV4,
+		Method:  "GET",
+		Expires: time.Now().Add(15 * time.Minute),
+	}
+	blob := client.Bucket(bucket).Object(object)
+
+	// Check if the object exists
+	_, err := blob.Attrs(ctx)
+	if err != nil {
+		fmt.Fprintf(w, "No object found: %s", http.StatusText(http.StatusNotFound))
+		return "", nil
+	}
+
+	u, err := client.Bucket(bucket).SignedURL(object, opts)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return "Error creating signed URL", fmt.Errorf("Bucket(%q).SignedURL: %w", bucket, err)
+	}
+
+	response := struct {
+		URL string `json:"url"`
+	}{
+		URL: u,
+	}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return "", fmt.Errorf("json.Marshal: %w", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+
+	return u, nil
+}
+
+func generateV4PutObjectSignedURL(w http.ResponseWriter, bucket, object string, client StorageClient) (string, error) {
+	opts := &storage.SignedURLOptions{
+		Scheme:  storage.SigningSchemeV4,
+		Method:  "PUT",
+		Expires: time.Now().Add(15 * time.Minute),
+	}
+
+	u, err := client.Bucket(bucket).SignedURL(object, opts)
+	if err != nil {
+		return "", fmt.Errorf("Bucket(%q).SignedURL: %w", bucket, err)
+	}
+
+	response := struct {
+		URL string `json:"url"`
+	}{
+		URL: u,
+	}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return "", fmt.Errorf("json.Marshal: %w", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+
+	return u, nil
+}
+
+func deleteObject(w http.ResponseWriter, bucket, object string, client StorageClient, ctx context.Context) (string, error) {
+	err := client.Bucket(bucket).Object(object).Delete(ctx)
+	if err != nil {
+		return "", fmt.Errorf("Bucket(%q).SignedURL: %w", bucket, err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Object deleted successfully"))
+
+	return "", nil
 }
