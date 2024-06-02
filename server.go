c02071efa28fb58519045b90ce2296b1d0ae9b7c
Filip Niklas <118931755+Firgrep@users.noreply.github.com>
2024-06-02 15:56:44 +0200

    test hello route

--- server.go
+++ server.go
@@ -63,6 +63,10 @@ func main() {
 	/*
 		Define server routes
 	*/
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World! From Caravan."))
+	})
 	http.HandleFunc("/read-object", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
